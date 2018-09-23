687d9fbeb163fce87117f3af303c9ac1b34f1ba1
Landon Schropp <schroppl@gmail.com>
2018-09-22 21:00:13 -0700

    Add static generator

---
+++ generators/static/index.js
@@ -0,0 +1,12 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class StaticGenerator extends Generator {
+
+  initializing() {
+    this.composeWith(require.resolve('../gitignore'));
+    this.composeWith(require.resolve('../package'));
+    this.composeWith(require.resolve('../eslint'));
+    this.composeWith(require.resolve('../babel'));
+    this.composeWith(require.resolve('../webpack'));
+  }
+};
